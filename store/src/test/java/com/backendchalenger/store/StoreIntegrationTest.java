df22d209273f1da29463277697170bbff3d89e45
andrelucasti <andrelucasti@gmail.com>
2019-08-08 23:43:34 -0300

    Improve Store Integration Tests

--- store/src/test/java/com/backendchalenger/store/StoreIntegrationTest.java
+++ store/src/test/java/com/backendchalenger/store/StoreIntegrationTest.java
@@ -3,23 +3,33 @@ package com.backendchalenger.store;
 import com.backendchalenger.store.model.Store;
 import com.backendchalenger.store.repository.StoreRepository;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 
-public class StoreIntegrationTest extends StoreApplicationTests{
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertSame;
 
-    private ExpectedException expectedException = ExpectedException.none();
+public class StoreIntegrationTest extends StoreApplicationTests{
 
     @Autowired
     private StoreRepository storeRepository;
 
     @Test(expected = DataIntegrityViolationException.class)
-    public void shouldNotCreateStoreWhenStoreExistWithSameName() {
+    public void shouldNotSaveStoreWhenStoreExistWithSameName() {
         final Store store1 = new Store("Store1", "My Fake Address Nº 123");
         storeRepository.save(store1);
 
         final Store storeSameName = new Store("Store1", "My Fake Address Nº 123");
         storeRepository.save(storeSameName);
+    }
+
+    @Test()
+    public void shouldSaveStoreWhenStoreRepositoryInvoked() {
+        final Store store1 = new Store("Store1", "My Fake Address Nº 123");
+        final Store storeSaved = storeRepository.save(store1);
+
+        assertNotNull(storeSaved.getId());
+        assertSame(store1.getAddress(), storeSaved.getAddress());
+        assertSame(store1.getName(), storeSaved.getName());
     }
 }
